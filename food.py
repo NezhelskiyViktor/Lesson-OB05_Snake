fea3a0c2d0e500787388fe0d3c6a467198efad33
NezhelskiyViktor <nezhelskiy@yandex.ru>
2024-04-29 22:20:02 +0300

    добавлена возможность продолжения игры по пробелу

--- food.py
+++ food.py
@@ -2,7 +2,6 @@ import pygame
 from random import *
 
 
-
 class Food(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -12,10 +11,18 @@ class Food(pygame.sprite.Sprite):
         self.food_img = pygame.image.load('image/food1.png').convert_alpha()
         # Загрузка изображения после инициализации
 
-    def update(self):
-        self.x = randint(0, 19) * 30 + 10
-        self.y = randint(0, 19) * 30 + 10
-        self.food_rect = pygame.Rect(self.x, self.y, 30, 30)
+    def update(self, snake):
+        repeat = True
+        while repeat:
+            self.x = randint(1, 19) * 30
+            self.y = randint(1, 19) * 30
+            self.food_rect = pygame.Rect(self.x, self.y, 30, 30)
+            for i in snake.body_list:
+                if self.food_rect.colliderect(i[1]):
+                    repeat = True
+                    break
+                else:
+                    repeat = False
 
     def draw(self, screen):
         screen.blit(self.food_img, (self.x, self.y))
--- main.py
+++ main.py
@@ -4,6 +4,44 @@ from food import Food
 from snake import Snake
 
 
+def game_proceccing(direction, snake, food, boundary_rect, screen, bg, takt):
+    # Обработка нажатий клавиш
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        direction = 'L'
+    elif keys[pygame.K_RIGHT]:
+        direction = 'R'
+    elif keys[pygame.K_UP]:
+        direction = 'U'
+    elif keys[pygame.K_DOWN]:
+        direction = 'D'
+
+    # Проверяем столкновение с границей экрана
+    game_continues = snake.body_list[0][1].colliderect(boundary_rect)
+
+    if not takt % 15:
+        # Проверка столкновения
+        collision = snake.body_list[0][1].colliderect(food.food_rect)
+        if collision:
+            food.update(snake)
+            snake.body_list.append(snake.body_list[-1])
+        if game_continues:
+            # Очистка экрана
+            screen.fill((130, 180, 50))
+        else:
+            screen.fill((0, 0, 0))
+            pygame.display.set_caption(
+                f'Змейка съела {len(snake.body_list) - 4} лягушек. \
+                Нажмите <пробел> для продолжения')
+        screen.blit(bg, (30, 30))
+        # Рисование snake и food
+        food.draw(screen)
+        if game_continues:
+            snake.move(screen, direction, shift)
+        else:
+            snake.draw(screen)
+    return direction
+
 pygame.init()
 pygame.mouse.set_visible(False)
 FPS = 60
@@ -21,9 +59,10 @@ pygame.display.set_caption('Змейка')
 shift = 30
 snake = Snake(screen_width // 2, screen_height // 2)
 food = Food()
-takt = 0
+food.update(snake)
 game_continues = True
 direction = 'D'
+takt = 0
 
 # Основной цикл программы
 running = True
@@ -32,42 +71,15 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-    # Обработка нажатий клавиш
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        direction = 'L'
-    elif keys[pygame.K_RIGHT]:
-        direction = 'R'
-    elif keys[pygame.K_UP]:
-        direction = 'U'
-    elif keys[pygame.K_DOWN]:
-        direction = 'D'
-
-    # Проверяем столкновение с границей экрана
-
-    if not takt % 15:
-        game_continues = snake.body_list[0][1].colliderect(boundary_rect)
-
-        # Проверка столкновения
-        collision = snake.body_list[0][1].colliderect(food.food_rect)
-        if collision:
-            food.update()
-            snake.body_list.append(snake.body_list[-1])
-        if game_continues:
-            # Очистка экрана
-            screen.fill((130, 180, 50))
-        else:
-            screen.fill((0, 0, 0))
-            pygame.display.set_caption('Игра окончена')
-        screen.blit(bg, (30, 30))
-        # Рисование snake и food
-        food.draw(screen)
-        if game_continues:
-            snake.move(screen, direction, shift)
-        else:
-            snake.draw(screen)
-
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                game_continues = True
+                pygame.display.set_caption('Змейка')
+                direction = 'D'
+                snake = Snake(screen_width // 2, screen_height // 2)
+                food = Food()
+                food.update(snake)
+    direction = game_proceccing(direction, snake, food, boundary_rect, screen, bg, takt)
     # Обновление экрана
     pygame.display.flip()
 
